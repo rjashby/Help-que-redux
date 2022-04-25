8b4177fa7c268897ffe5adfe37153c1265334ff1
Ryan Ashby <rjapsu160@gmail.com>
2022-04-25 11:50:22 -0700

    update ticket control methods to work with redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,14 +4,15 @@ import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
+import PropTypes from "prop-types";
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
       formVisibleOnPage: false,
-      mainTicketList: [],
       selectedTicket: null,
       editing: false
     };
@@ -31,37 +32,87 @@ class TicketControl extends React.Component {
     }
   }
 
+  // OLD ADD METHOD
+  // handleAddingNewTicketToList = (newTicket) => {
+  //   const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+  //   this.setState({
+  //     mainTicketList: newMainTicketList,
+  //     formVisibleOnPage: false
+  //   });
+  // }
+
   handleAddingNewTicketToList = (newTicket) => {
-    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
-    this.setState({
-      mainTicketList: newMainTicketList,
-      formVisibleOnPage: false
-    });
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = newTicket;
+    const action = {
+      type: 'ADD_TICKET',
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    }
+    dispatch(action);
+    this.setState({formVisibleOnPage: false});
   }
 
+  // OLD SELECTED TICKET METHOD
+  // handleChangingSelectedTicket = (id) => {
+  //   const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+  //   this.setState({selectedTicket: selectedTicket});
+  // }
+
   handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    const selectedTicket = this.props.mainTicketList[id];
     this.setState({selectedTicket: selectedTicket});
   }
 
+  // OLD DELETE METHOD
+  // handleDeletingTicket = (id) => {
+  //   const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
+  //   this.setState({
+  //     mainTicketList: newMainTicketList,
+  //     selectedTicket: null
+  //   });
+  // }
+
   handleDeletingTicket = (id) => {
-    const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
-    this.setState({
-      mainTicketList: newMainTicketList,
-      selectedTicket: null
-    });
+    const { dispatch } = this.props;
+    const action = {
+      type: 'DELETE_TICKET',
+      id: id
+    }
+    dispatch(action);
+    this.setState({selectedTicket: null});
   }
 
   handleEditClick = () => {
     this.setState({editing: true});
   }
 
+  // OLD EDIT METHOD
+  // handleEditingTicketInList = (ticketToEdit) => {
+  //   const editedMainTicketList = this.state.mainTicketList
+  //     .filter(ticket => ticket.id !== this.state.selectedTicket.id)
+  //     .concat(ticketToEdit);
+  //   this.setState({
+  //     mainTicketList: editedMainTicketList,
+  //     editing: false,
+  //     selectedTicket: null
+  //   });
+  // }
+
   handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = this.state.mainTicketList
-      .filter(ticket => ticket.id !== this.state.selectedTicket.id)
-      .concat(ticketToEdit);
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = ticketToEdit;
+    const action = {
+      type: 'ADD_TICKET',
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    }
+    dispatch(action);
     this.setState({
-      mainTicketList: editedMainTicketList,
       editing: false,
       selectedTicket: null
     });
@@ -97,6 +148,18 @@ class TicketControl extends React.Component {
 
 }
 
-TicketControl = connect()(TicketControl);
+TicketControl.propTypes = {
+  mainTicketList: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    mainTicketList: state
+  }
+}
+
+// Note: we are now passing mapStateToProps into the connect() function.
+
+TicketControl = connect(mapStateToProps)(TicketControl);
 
 export default TicketControl;
